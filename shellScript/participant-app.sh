f7a8cc60a73331f7a67fa7265ccb22bcb5f927b3
yassi-github <s19t312@kagawa-u.ac.jp>
2021-11-23 18:48:49 +0900

    :bug: Fix POST destination url path.

--- shellScript/participant-app.sh
+++ shellScript/participant-app.sh
@@ -5,5 +5,5 @@
 #SERVERADDR='192.0.2.10:xxxx'
 SERVERADDR=$1
 
-curl -X POST -d "$(<ProjectHome>/shellScript/show-participants-data.sh)" ${SERVERADDR}/save/
+curl -X POST -d "$(<ProjectHome>/shellScript/show-participants-data.sh)" ${SERVERADDR}/participants
 
