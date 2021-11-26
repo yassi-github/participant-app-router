855a460486f8325140938fe11f369846dfa20014
yassi-github <s19t312@kagawa-u.ac.jp>
2021-11-26 20:10:50 +0900

    :necktie: Add loop POST process every 5sec for 1min.

--- shellScript/participant-app.sh
+++ shellScript/participant-app.sh
@@ -5,5 +5,13 @@
 #SERVERADDR='192.0.2.10:xxxx'
 SERVERADDR=$1
 
+# loop till **:00
+while [[ "$(date +%S)" != "00" ]]; do
+
+# POST
 curl -X POST -d "$(<ProjectHome>/shellScript/show-participants-data.sh)" ${SERVERADDR}/participants
 
+# wait a few seconds
+sleep 5s
+
+done
